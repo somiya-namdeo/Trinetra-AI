7a3fe6e6a089f80bd0eabbdfad2f79b3247f57d7
SomiyaNamdeo <namdeosomiya2005@gmail.com>
2026-06-07 21:52:04 +0530

    Fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://trinetra-ai-eight.vercel.app/",
+        "https://trinetra-ai-eight.vercel.app",
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173"
